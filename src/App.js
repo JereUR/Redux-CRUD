0d71aa361c95f9bc23a2ab20618dabf676c8791b
Jeremias Dominguez Vega <jeremias.jdv@gmail.com>
2023-01-12 19:39:39 -0300

    Example with shopping cart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Provider } from "react-redux";
 import { Counter } from "./components/Counter";
+import ShoppingCart from "./components/ShoppingCart";
 
 import TeoriaRedux from "./components/TeoriaRedux";
 import store from "./store";
@@ -8,13 +9,14 @@ function App() {
   return (
     <Provider store={store}>
       <div style={{ textAlign: "center" }}>
-      <h1>Redux</h1>
-      <Counter/>
-      <hr />
-      <TeoriaRedux />
-    </div>
+        <h1>Redux</h1>
+        <ShoppingCart />
+        <hr />
+        <Counter />
+        <hr />
+        <TeoriaRedux />
+      </div>
     </Provider>
-    
   );
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import counterReducer from "./counterReducer";
+import { shoppingReducer } from "./shoppingReducer";
 
 const reducer = combineReducers({
   counter: counterReducer,
+  shopping: shoppingReducer,
 });
 
 export default reducer;
